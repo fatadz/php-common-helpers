091f0be1a1fc82a70bc1c3bfa97ce2795661aac1
fahmi <fahmimiftah.faridz@gmail.com>
2021-02-17 21:55:20 +0700

    add gitignore

--- randomStr.php
+++ randomStr.php
@@ -4,10 +4,10 @@ function randomStr($lower_n = 5, $upper_n = 1, $number_n = 2, $punct_n = 0) { //
     $number = '0123456789';
     $punct  = '!@#$%^&*()_-=+;:,.?';
 
-        $lower_p  = substr (str_shuffle ($lower), 0, $lower_n);
-        $upper_p  = substr (str_shuffle ($upper), 0, $upper_n);
-	    $number_p = substr (str_shuffle ($number), 0, $number_n);
-	    $punct_p  = substr (str_shuffle ($punct), 0, $punct_n);
+    $lower_p  = substr (str_shuffle ($lower), 0, $lower_n);
+    $upper_p  = substr (str_shuffle ($upper), 0, $upper_n);
+    $number_p = substr (str_shuffle ($number), 0, $number_n);
+    $punct_p  = substr (str_shuffle ($punct), 0, $punct_n);
 
-	        return str_shuffle ($lower_p.$upper_p.$number_p.$punct_p);
-	    }
+    return str_shuffle ($lower_p.$upper_p.$number_p.$punct_p);
+}
